ebca590b1f7602cdcd2a42b5608fd131e7808b95
Tesh-nician <145364823+Tesh-nician@users.noreply.github.com>
2023-10-11 12:04:34 +0200

    Add files via upload

---
+++ Java_Fundementals/Chapter03_Classes/src/Les1/Oefening1/Oefening1.java
@@ -0,0 +1,15 @@
+package Les1.LesOefeningen1;
+
+public class Oefening1 {
+    public static void main(String[] args) {
+
+        double[] dim = new double[10];
+
+        char[] dim2 = {'a','b', 'c', 'd', 'e'};
+
+        System.out.println(dim[2]);
+        System.out.println(dim2[2]);
+
+
+    }
+}
