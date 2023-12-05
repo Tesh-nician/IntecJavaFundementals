30be5d69bad97b9711568bf66630dfe9695e2b7e
Tesh-nician <145364823+Tesh-nician@users.noreply.github.com>
2023-12-05 17:57:50 +0100

    Add files via upload

---
+++ Java_Fundementals/Chapter03_Classes/src/Opdracht1/RobotApp.java
@@ -0,0 +1,15 @@
+package Java_Fundementals.Chapter07_Abstraction.src.Opdracht1;
+
+public class RobotApp {
+    public static void main(String[] args) {
+        BendingRobot Bender1 = new BendingRobot("Charlie", 180);
+
+
+
+
+        Bender1.bend(50);
+        Bender1.bend(150);
+    }
+
+
+}
